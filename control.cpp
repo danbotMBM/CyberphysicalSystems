66419ab31b896ac9848d346227d1311b95674844
danbotMBM <danielmarkjones9@gmail.com>
2023-01-27 14:12:19 -0600

    speed and angle control finished. Outputting forward distance aswell

--- control.cpp
+++ control.cpp
@@ -20,7 +20,8 @@ static double kp = 250.0;
 static double kd = 0.0;
 static double servo_offset = 18.5;
 static double prev_error = 0.0;
-static double vel_input = 25.0;
+static double vel_input = 0.0;
+static double ang_input = 0.0;
 static double start_time = 0.0;
 static int choice = 0;	// choose spin mode
 
@@ -33,41 +34,14 @@ void callback(const boost::shared_ptr<const race::pid_input> &data, ros::Publish
 	// auto new_start = std::chrono::high_resolution_clock::now();
 	// std::chrono::duration<double> elapsed = new_start - end_time;
 	// cout << "Elapsed time between callback: " << (double)elapsed.count() << "s"<<endl;
-	double angle = data.get()->pid_error*kp;
+
+	// double angle = data.get()->pid_error*kp;
+	double angle = ang_input;
+	double velocity = vel_input;
+
 	race::drive_param msg;
 
-	string command;
-	cout << "Enter command: ";
-	//TODO parse the string
-	string temp;
-	char command_type;
-	cin >> temp;
-	command_type = temp.at(0);
-
-	double value;
-	cin >> temp;
-	value = stod(temp);
-	//TODO HAVE SAFETY MEASURES for weird inputs
-	switch (command_type){
-		case 's':
-			vel_input = value;
-			cout << "Velocity set to " << vel_input << endl;
-		break;
-		case 't':
-			angle = value;
-			cout << "Angle set to " << angle << endl;
-		break;
-		case 'c':
-			cout << "c case";
-		break;
-		default:
-			vel_input = 0;
-			cout << "default case";
-		break;
-	}
-
-
-	
+	// cout << "\ndata.get()\n\n" << *data.get() << "\n" << endl;
 
 	if (angle > 100){
 		angle = 100;
@@ -76,13 +50,16 @@ void callback(const boost::shared_ptr<const race::pid_input> &data, ros::Publish
 		angle = -100;
 	}
 	
-	if(data.get()->pid_vel == 0){
-		msg.velocity = -8;
-	}
-	else{
-		msg.velocity = vel_input;
-	}	
+	// if(data.get()->pid_vel == 0){
+	// 	msg.velocity = -8;
+	// }
+	// else{
+	// 	msg.velocity = vel_input;
+	// }	
+
+	msg.velocity = velocity;
 	msg.angle = angle;
+
 	cout << "Velocity: " << msg.velocity << " Angle: " << msg.angle << endl;
 
 	pub.publish(msg);
@@ -103,9 +80,9 @@ int main(int argc, char **argv){
 	string command_s2;
 	
 	// Get user input
-	cout << "Listening to error for PID" << "\n";
-	cout << "Enter Velocity: ";
-	cin >> vel_input;
+	// cout << "Listening to error for PID" << "\n";
+	// cout << "Enter Velocity: ";
+	// cin >> vel_input;
 	/*
 	// Chose different types of spin
 	cout << "Choose spinning method(0--Robust Spin, 1--Dynamic Spin, 2--Static Spin): ";
@@ -129,37 +106,94 @@ int main(int argc, char **argv){
 	ros::NodeHandle s;
 	ros::Subscriber sub = s.subscribe<race::pid_input>("error", sub_queue_size, boost::bind(callback, _1, pub));
 
+	bool run = true;
+	while(run){
+		race::drive_param msg;
+		string command;
+		cout << "Enter command: ";
+		//TODO parse the string
+		// string temp;
+		char command_type;
+		cin >> command;
+		command_type = command.at(0);
 
-	// Robust Spin
-	if(choice == 0){
-		ros::spin();
-	}
-
-	// Dynamic Spin
-	else if(choice == 1){
-		while(ros::ok()){
-			// record the time in millisecond that used for the callback function
-			auto start = std::chrono::high_resolution_clock::now();
-
-			ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.01));
-
-			auto end = std::chrono::high_resolution_clock::now();
-			std::chrono::duration<double> elapsed = end - start;
-			int sleep_time = (int)(given_time - (double)elapsed.count()*1000);
-
-			// Dynamically sleep
-			std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+		double value;
+		
+		//TODO HAVE SAFETY MEASURES for weird inputs
+		switch (command_type){
+			case 's':
+				cin >> command;
+				value = stod(command);
+				vel_input = value;
+				cout << "Velocity set to " << vel_input << endl;
+			break;
+			case 't':
+				cin >> command;
+				value = stod(command);
+				ang_input = value;
+				cout << "Angle set to " << ang_input << endl;
+			break;
+			case 'c':
+				vel_input = 12;
+				ang_input = 100;
+				cout << "Velocity set to " << vel_input << " and Angle set to " << ang_input << endl;
+			break;
+			case 'h':
+				vel_input = 0;
+				ang_input = 0;
+				cout << "Car stopped" << endl;
+			break;
+			case 'q':
+				vel_input = 0;
+				ang_input = 0;
+				cout << "Car stopped. Exiting..." << endl;
+				run = false;
+			default:
+				vel_input = 0;
+				ang_input = 0;
+				cout << "Default case. Stop the car and exit." << endl;
+				run = false;
+			break;
 		}
+		msg.velocity = vel_input;
+		msg.angle = ang_input;
+		//TODO Check angle
+		pub.publish(msg);
+		//CHECKS CALLBACK once
+		ros::spinOnce();
 	}
 
-	// Static Spin
-	else if(choice == 2){
-		ros::Rate r(hz);
-		while (ros::ok()){
-			ros::spinOnce();
-			r.sleep();
-		}
-	}
+
+	// // Robust Spin
+	// if(choice == 0){
+	// 	ros::spin();
+	// }
+
+	// // Dynamic Spin
+	// else if(choice == 1){
+	// 	while(ros::ok()){
+	// 		// record the time in millisecond that used for the callback function
+	// 		auto start = std::chrono::high_resolution_clock::now();
+
+	// 		ros::getGlobalCallbackQueue()->callAvailable(ros::WallDuration(0.01));
+
+	// 		auto end = std::chrono::high_resolution_clock::now();
+	// 		std::chrono::duration<double> elapsed = end - start;
+	// 		int sleep_time = (int)(given_time - (double)elapsed.count()*1000);
+
+	// 		// Dynamically sleep
+	// 		std::this_thread::sleep_for(std::chrono::milliseconds(sleep_time));
+	// 	}
+	// }
+
+	// // Static Spin
+	// else if(choice == 2){
+	// 	ros::Rate r(hz);
+	// 	while (ros::ok()){
+	// 		ros::spinOnce();
+	// 		r.sleep();
+	// 	}
+	// }
 	
 	
 
