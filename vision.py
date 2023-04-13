91e04e0e7c9a258e0c10e63b20ec5ca08704c28c
danbotMBM <danielmarkjones9@gmail.com>
2023-04-13 15:43:58 -0500

    subscribe to both images

--- vision.py
+++ vision.py
@@ -9,26 +9,57 @@ from cv_bridge import CvBridge
 ros.init_node("image_listener", anonymous=True)
 bridge = CvBridge()
 
+color_recv = False
+depth_recv = False
+color_img = None
+depth_img = None
+
 def img_callback(msg, pub):
-    print("callback")
+    global color_img
+    global color_recv
+    print("img_callback")
     img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-    cv.imshow("name", img)
-    cv.waitKey(1)
+    color_img = img
+    color_recv = True
+    #print(img.shape, img)
+    display()
+
 
 def depth_callback(msg, pub):
-    print("callback")
+    global depth_recv
+    global depth_img
+    print("depth callback")
     img = bridge.imgmsg_to_cv2(msg, desired_encoding="passthrough")
-    print(img.shape, type(img[0][0]))
-    img = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
-    cv.imshow("name", img)
-    map = cv.applyColorMap(img, cv.COLORMAP_RAINBOW)
-    cv.imshow("depth Colored", map)
+    depth_img = img
+    depth_recv = True
+    #print(img.shape, type(img[0][0]))
+    #print(img)
+    display()
+    #img = cv.normalize(img, None, 0, 255, cv.NORM_MINMAX, cv.CV_8UC1)
+
+    # map = cv.applyColorMap(img, cv.COLORMAP_RAINBOW)
+    #cv.imshow("depth Colored", img)
+    #cv.waitKey(1)
+
+
+def display():
+    depth_recv = True
+    if color_recv and depth_recv:
+        print(color_img.shape, type(color_img[0][0][0]), color_img)
+        print(depth_img.shape, type(depth_img[0][0]), depth_img)
+    	#cv.imshow("depth", depth_img)
+    	#cv.imshow("color", color_img)
+  
+        
     cv.waitKey(1)
 
 
 def main():
     pub = ros.Publisher("vision/imageStuff", String, queue_size=1000)
-    sub = ros.Subscriber("depth/depth_raw_registered", Image, depth_callback, (pub))
+    
+    ros.Subscriber("depth/depth_raw_registered", Image, depth_callback, (pub))
+    ros.Subscriber("rgb/image_rect_color", Image, img_callback, (pub))
+    
     ros.spin()
 
 if __name__ == '__main__':
